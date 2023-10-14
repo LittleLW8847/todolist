b48e19c1ba58e24fb7f05a1a56b294d1fa43a5f9
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2023-10-14 16:31:21 +0700

    okay now i can use a props to send task value

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Todoform from './Todoform'
+import {v4 as uuidv4} from 'uuid'
+uuidv4();
 
-function Todo() {
+function Todo(){
+    const [toDos, setToDo] = useState([]); 
+
+    const addToDo = todo =>{
+
+      if(!todo.text || /^\s*$/.test(todo.text)){
+        return;
+      }
+
+      const newToDos = [todo, ...toDos]
+        setToDo(newToDos);
+    }
+    
   return (
     <div className='todoWrapper'>
-        <Todoform/>
+        <Todoform onSubmit = {addToDo}/>
     </div>
   )
 }
 
-export default Todo
+
+export default Todo;
--- src/component/Todoform.js
+++ src/component/Todoform.js
@@ -1,25 +1,28 @@
 import React, {useState} from 'react'
 
-function Todoform() {
+function Todoform(props) {
 
     const [taskValue, setTaskValue] = useState('')
 
     const handleSubmit = (e) => {
       e.preventDefault();
 
-      console.log("The task is" + taskValue);
+      props.onSubmit({//create props to passing task
+        id: Math.floor(Math.random() * 1000),
+        text: taskValue
+      });
     }
 
     const handleChange = (e) =>{
       setTaskValue(e.target.value);
     }
 
-
   return (
     <form onSubmit={handleSubmit}>
         <input type='text' 
         placeholder='What do you want to do?' 
         name='text' 
+        value={taskValue}
         className='todo-form-input' 
         onChange={handleChange}
         >
@@ -27,6 +30,7 @@ function Todoform() {
         <button className='addTaskbtn' type='submit' on>Add task</button>
     </form>
   )
+  
 }
 
 export default Todoform
